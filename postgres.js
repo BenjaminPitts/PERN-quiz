d849fd9fc88ff96060a3b160e1e592c0c6532cf6
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-02-15 14:02:21 -0700

    added ?ssl=true

--- postgres.js
+++ postgres.js
@@ -1,7 +1,7 @@
 const Client = require('pg').Client
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/quiz_backend_development',
+    connectionString: process.env.DATABASE_URL?ssl=true || 'postgresql://localhost:5432/quiz_backend_development',
 })
 console.log(process.env.DATABASE_URL);
 
