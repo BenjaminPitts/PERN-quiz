f1c1de8abe21929624bf097629b1d7d2ba2934dd
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-02-15 13:08:26 -0700

    how bout now

--- public/app.js
+++ public/app.js
@@ -20,7 +20,7 @@ class App extends React.Component {
 
     handleSubmit = (event) => {
       // event.preventDefault()
-      axios.post('/', this.state).then((response) => {
+      axios.post('/api', this.state).then((response) => {
         console.log(response.data)
         this.setState({
           quizq: [response.data],
@@ -36,20 +36,20 @@ class App extends React.Component {
       event.preventDefault()
       event.target.reset()
       const id = event.target.id
-      axios.put('/' + id, this.state).then((response) => {
+      axios.put('/api/' + id, this.state).then((response) => {
         this.getQuestion()
       })
     }
 
     deleteQuestion = (event) => {
-      axios.delete('/' + event.target.value).then((response) => {
+      axios.delete('/api/' + event.target.value).then((response) => {
         this.getQuestion()
       })
     }
 
     getQuestion = () => {
       axios
-      .get('/')
+      .get('/api')
       .then(
         (response) => this.setState({
           quizq: response.data,
