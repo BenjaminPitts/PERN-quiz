1c953d4351423edcaa159a23f14f818fd11951ff
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-02-18 11:55:34 -0700

    try

--- server.js
+++ server.js
@@ -6,14 +6,10 @@ app.use(express.json());
 app.use(express.static('public'))
 
 const quizController = require('./controllers/quiz.js');
- app.use('/quiz', quizController);
+ app.use('https://quiz-js-pernstack.herokuapp.com/quiz', quizController);
 
 postgres.connect();
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('listening');
 })
-
-
-
-// https://quiz-js-pernstack.herokuapp.com/
