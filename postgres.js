694bcdc4551f878206cc04102323ae7537b8e406
Benjamin Pitts <benjaminpitts@Benjamins-MacBook-Pro.local>
2021-02-18 10:35:57 -0700

    host

--- postgres.js
+++ postgres.js
@@ -1,7 +1,13 @@
 const Client = require('pg').Client
 
 const client = new Client({
-    connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/quiz_backend_development',
+    //connectionString: process.env.DATABASE_URL || 'postgresql://localhost:5432/quiz_backend_development',
+    user: "admin",
+    password: "guest",
+    database: "quiz",
+    port: 5432,
+    host: process.env.DATABASE_URL || 'postgresql://localhost:5432/quiz_backend_development',
+    ssl: true
 })
 console.log(process.env.DATABASE_URL);
 
